24ca02c230ce8b48e8aaf09cc8f38d8d329dd1bd
Tomexar <tommyexar@gmail.com>
2019-05-02 17:23:53 -0600

    finished styling

--- instagram-app/src/components/CommentSection/CommentInput.js
+++ instagram-app/src/components/CommentSection/CommentInput.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FormBar = styled.form`
+border-top:1px solid lightgrey;
+margin-top:15px;
+width:600px;
+`;
+
+
+const CommentBar = styled.input`
+    outline: 0;
+    border:0;
+    height:55px;
+`;
+
 
 function CommentInput(props) {
     return(
-        <form onSubmit = {props.submit}>
-            <input type = 'text' placeholder = 'Add Comment' value = {props.comment} onChange = {props.handler}/>
-        </form>
+        <FormBar onSubmit = {props.submit}>
+            <CommentBar type = 'text' placeholder = 'Add Comment' value = {props.comment} onChange = {props.handler}/>
+        </FormBar>
     )
 }
 
